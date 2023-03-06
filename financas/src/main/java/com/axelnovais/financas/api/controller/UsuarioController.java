ff66d19dd1dfcb415a1cac1a61cc8c679efb8671
FelipeAxel <axelnovais96@gmail.com>
2023-03-06 17:27:46 -0300

    Created method post the users

--- financas/src/main/java/com/axelnovais/financas/api/controller/UsuarioController.java
+++ financas/src/main/java/com/axelnovais/financas/api/controller/UsuarioController.java
@@ -1,13 +1,34 @@
 package com.axelnovais.financas.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.axelnovais.financas.dto.UsuarioDTO;
+import com.axelnovais.financas.exceptions.RegraDeNogocioException;
+import com.axelnovais.financas.model.entity.Usuario;
+import com.axelnovais.financas.services.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioController {
 
-    @GetMapping("/")
-    public String heloWord(){
-        return "Hello word";
+    private UsuarioService service;
+    public UsuarioController(UsuarioService service){
+        this.service =service;
     }
+            @PostMapping
+            public ResponseEntity salvar(@RequestBody UsuarioDTO dto ){
+                Usuario usuario = Usuario.builder()
+                        .nome(dto.getNome())
+                        .email(dto.getEmail())
+                        .senha((dto.getSenha()))
+                        .build();
+
+                try{
+                    Usuario usuarioSalvo = service.salvarUsuario(usuario);
+                    return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+                }catch(RegraDeNogocioException e){
+                    return ResponseEntity.badRequest().body(e.getMessage());
+                }
+            }
 }
