a7292aa9a1936f33f3e1e82ed34a1b8decdf70dd
FelipeAxel <axelnovais96@gmail.com>
2023-03-21 09:27:32 -0300

    Front-end

---
+++ minhas-financas-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Login from './views/login';
+
+import 'bootswatch/dist/flatly/bootstrap.css' 
+
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
