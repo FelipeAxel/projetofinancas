c2705f022a32d89c9428a19eb68d5f3cfdbc442e
FelipeAxel <axelnovais96@gmail.com>
2023-03-23 13:55:40 -0300

    Config CORS the Application and test require DB

--- financas/src/main/java/com/axelnovais/financas/FinancasApplication.java
+++ financas/src/main/java/com/axelnovais/financas/FinancasApplication.java
@@ -2,14 +2,54 @@ package com.axelnovais.financas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 @EnableSwagger2
-public class FinancasApplication {
+@EnableWebMvc
+public class FinancasApplication implements WebMvcConfigurer {
 
+
+	public void addCorsMapping(CorsRegistry registry){
+		registry.addMapping("/* *").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FinancasApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+
+		List<String> all = Arrays.asList("*");
+
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedMethods(all);
+		config.setAllowedOriginPatterns(all);
+		config.setAllowedHeaders(all);
+		config.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**",  config);
+
+		CorsFilter corFilter = new CorsFilter(source);
+
+		FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<CorsFilter>(corFilter);
+		filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+		return filter;
+
 	}
 
 }
--- minhas-financas-app/src/views/login.js
+++ minhas-financas-app/src/views/login.js
@@ -2,65 +2,78 @@ import React from 'react'
 import Card from '../components/card'
 import FormGroup from '../components/form-group'
 import { withRouter } from 'react-router-dom'
+import axios from 'axios'
 class Login extends React.Component {
 
-    state={
-        email:'',
-        senha:''
+    state = {
+        email: '',
+        senha: '',
+        mensagemErro:null
     }
 
-    entrar = ()=>{
-        console.log('Email: ', this.state.email)
-        console.log('Senha: ', this.state.senha)
+    entrar = () => {
+        axios
+            .post('http://localhost:8080/api/usuarios/autenticar',
+                {
+                    email: this.state.email,
+                    senha: this.state.senha
+                }).then(response => {
+                    this.props.history.push('/home')
+                }).catch(erro=>{
+                    this.setState({mensagemErro: erro.response.data});
+                })
 
     }
 
-    prepararCadastrar =()=>{
+    prepararCadastrar = () => {
         this.props.history.push('/cadastro-usuarios')
     }
 
     render() {
         return (
-                <div className="row">
-                    <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
-                        <div className="bs-docs-section">
-                            <Card title="Login">
-                                <div className="row">
-                                    <div className="col-lg-12">
-                                        <div className="bs-component">
-                                            <form>
-                                                <fieldset>
-                                                   <FormGroup label = "Email: *" htmlFor = "exampleInputEmail1">
-                                                   <input type="email" 
-                                                          value={this.state.email}
-                                                          onChange={e=>this.setState({email: e.target.value})}
-                                                          className="form-control" 
-                                                          id="exampleInputEmail1" 
-                                                          aria-describedby="emailHelp"
-                                                          placeholder="Email"/>
-                                                   </FormGroup>
+            <div className="row">
+                <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
+                    <div className="bs-docs-section">
+                        <Card title="Login">
+                            <div className="row">
+                                <span>{this.state.mensagemErro}</span>
+                            </div>
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="bs-component">
+                                        <form>
+                                            <fieldset>
+                                                <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
+                                                    <input type="email"
+                                                        value={this.state.email}
+                                                        onChange={e => this.setState({ email: e.target.value })}
+                                                        className="form-control"
+                                                        id="exampleInputEmail1"
+                                                        aria-describedby="emailHelp"
+                                                        placeholder="Email" />
+                                                </FormGroup>
 
-                                                   <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
-                                                   <input type="password" 
-                                                          value={this.state.senha}
-                                                          onChange={e=>this.setState({senha: e.target.value})}
-                                                          className="form-control" 
-                                                          id="exampleInputPassword1" 
-                                                          placeholder="Password"/>
-                                                   </FormGroup>
-                                                   <button onClick={this.entrar} className="btn btn-success">Entrar</button>
-                                                   <button onClick= {this.prepararCadastrar}className="btn btn-danger">Cadastrar</button>
+                                                <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                                                    <input type="password"
+                                                        value={this.state.senha}
+                                                        onChange={e => this.setState({ senha: e.target.value })}
+                                                        className="form-control"
+                                                        id="exampleInputPassword1"
+                                                        placeholder="Password" />
+                                                </FormGroup>
+                                                <button onClick={this.entrar} className="btn btn-success">Entrar</button>
+                                                <button onClick={this.prepararCadastrar} className="btn btn-danger">Cadastrar</button>
                                             </fieldset>
                                         </form>
-                                        </div>
                                     </div>
                                 </div>
-                            </Card>
-                        </div>
+                            </div>
+                        </Card>
                     </div>
                 </div>
+            </div>
         )
     }
 }
 
-export default withRouter( Login )
+export default withRouter(Login)
