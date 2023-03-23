bc21d25eb9d54d39afc6cb6a48f1ca4809948dc3
FelipeAxel <axelnovais96@gmail.com>
2023-03-23 20:28:22 -0300

    add toastr, bug fix CORS, folder App

--- minhas-financas-app/src/main/App.js
+++ minhas-financas-app/src/main/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 import Rotas from './rotas';
 import NavBar from '../components/navBar';
+import 'toastr/build/toastr.min'
+
 import 'bootswatch/dist/flatly/bootstrap.css'
 import '../custom.css'
+import 'toastr/build/toastr.css'
 
 function App() {
   return (
--- minhas-financas-app/src/views/home.js
+++ minhas-financas-app/src/views/home.js
@@ -1,5 +1,6 @@
 import React from "react";
-import axios from 'axios'
+import UsuarioService from "../app/service/usuarioService";
+import LocalStorageService from "../app/service/localstorageService";
 
 class Home extends React.Component {
 
@@ -7,9 +8,17 @@ class Home extends React.Component {
         saldo: 0
     }
 
+    constructor(){
+        super()
+        this.usuarioService = new UsuarioService();
+    }
         componentDidMount(){
-            axios.get('http://localhost:8080/api/usuarios/3/saldo')
-            .then(response=>{
+         
+            const usuarioLogado = LocalStorageService.obterItem('_usuario_logado')
+
+           this.usuarioService
+           .obterSaldoPorUsuario(usuarioLogado.id)
+           .then(response=>{
                 this.setState({saldo: response.data})
             }).catch(error=>{
                 console.error(error.response)
